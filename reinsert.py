1464e474c2078db98b5fdd2408f16e7f2791363c
Ben Ferguson <bent@Bens-MacBook-Air.local>
2025-06-22 21:56:24 -0400

    search in tl, changed caps reinsert

--- reinsert.py
+++ reinsert.py
@@ -25,9 +25,9 @@ class TLWord():
         self.original = []
         self.translation = _tl
         #self.count = 1
-        if(buff == True):
-            if (len(self.translation)) % 2 != 0:
-                self.translation += " "
+        #if(buff == True):
+        #    if (len(self.translation)) % 2 != 0:
+        #        self.translation += " "
     ###
 ###
 
@@ -76,67 +76,67 @@ for w in js['words']:
     ###
 ##
 
-words.append(TLWord(0x67a8, 10, "Half-Elf (Elf)")) # 10 spaces
-words.append(TLWord(0x679d, 10, "Half-Elf (Hum)")) # 10 spaces
+words.append(TLWord(0x299ca, 18, "Is this OK?        Y       N "))
+words.append(TLWord(0x67a8, 10, "Half-Elf(El)")) # 10 spaces
+words.append(TLWord(0x679d, 10, "Half-Elf(Hu)")) # 10 spaces
 words.append(TLWord(0x8cbc, 11, "Item        Stats"))
-words.append(TLWord(0x8cc8, 12, "Magic       System"))
+words.append(TLWord(0x8cc8, 12, "Magic       System "))
 words.append(TLWord(0x8cd5, 16, "Equipment   Options"))
-words.append(TLWord(0x2cb40, 2, "Pt"))
+
 words.append(TLWord(0x1b1e0, 2, "En"))
 
 words.append(TLWord(0x5f07c, 29, "Oran is the largest city \x01on all of Alecrast."))
-words.append(TLWord(0x2993b, 16, "Choose a race:"))
-words.append(TLWord(0x15932, 10, "Adventure on"))
-words.append(TLWord(0x159f1, 10, "Adventure on"))
+words.append(TLWord(0x2993b, 16, "Choose a race: "))
+words.append(TLWord(0x15932, 10, "Adventure on "))
+words.append(TLWord(0x159f1, 10, "Adventure on "))
 words.append(TLWord(0x1593f, 10, "New character"))
 words.append(TLWord(0x1594a, 13, "Erase adventure"))
-words.append(TLWord(0x1595a, 14, "Copy adventure"))
-words.append(TLWord(0x16439, 14, "Copy adventure"))
+words.append(TLWord(0x1595a, 14, "Copy adventure "))
+words.append(TLWord(0x16439, 14, "Copy adventure "))
 words.append(TLWord(0x15afb, 10, "New character"))
 words.append(TLWord(0x16420, 10, "New character"))
 words.append(TLWord(0x1642b, 13, "Erase adventure"))
 words.append(TLWord(0x15d38, 17, "Erase save?     Yes   No"))
 words.append(TLWord(0x15d4a, 13, "Erase adventure"))
-words.append(TLWord(0x153b7, 41, "Save game   Formatn.Message history \x01Toss item "))
+words.append(TLWord(0x153b7, 41, "Save game  Formatn.Message history \x01Toss item "))
 
 words.append(TLWord(0x165d0, 7, "Empty"))
 words.append(TLWord(0x2994e, 3, "Hum"))
 words.append(TLWord(0x29950, 3, "Elf"))
-words.append(TLWord(0x29954, 6, "Dwarf"))
-words.append(TLWord(0x29958, 8, "Grassrunner"))
-words.append(TLWord(0x29965, 13, "Half Elf   -    Human"))
+words.append(TLWord(0x29954, 6, "Dwarf  "))
+words.append(TLWord(0x29958, 8, "  Grassrunner"))
+words.append(TLWord(0x29965, 13, "Half-Elf   -  Human"))
 words.append(TLWord(0x29973, 7, "Elven"))
 
-words.append(TLWord(0x299b4, 7, "MaleFem"))
+words.append(TLWord(0x299b4, 7, "MalFem"))
 
-words.append(TLWord(0x299ca, 18, "Is this OK?         Yes     No"))
-words.append(TLWord(0x19c70, 4,  "Songs"))
+words.append(TLWord(0x19c70, 4,  "Song"))
 
-words.append(TLWord(0x299e7, 4, " Born"))
-words.append(TLWord(0x299eb, 6, " Exp."))
-words.append(TLWord(0x299f0, 6, "Gamel"))
+words.append(TLWord(0x299e7, 4, " Born "))
+words.append(TLWord(0x299eb, 6, " Exp. "))
+words.append(TLWord(0x299f0, 6, "Gam. "))
 
 words.append(TLWord(0x29a46, 15, "Raise which skills?"))
 words.append(TLWord(0xb52f, 6, "Exp. "))
 
 words.append(TLWord(0x29cc6, 4, "Wild"))
 words.append(TLWord(0x29cce, 7, "Magician"))
-words.append(TLWord(0x29cd9, 4, "Thug"))
+words.append(TLWord(0x29cd9, 4, "Thug "))
 words.append(TLWord(0x29ce1, 3, "Trav"))
-words.append(TLWord(0x29ce8, 3, "Hunt"))
-words.append(TLWord(0x29cef, 7, "Civilian"))
-words.append(TLWord(0x29cfa, 8, "Gentry"))
+words.append(TLWord(0x29ce8, 3, "Hnt"))
+words.append(TLWord(0x29cef, 7, "Civilian "))
+words.append(TLWord(0x29cfa, 8, "Gentry "))
 words.append(TLWord(0x29d06, 4, "Merc"))
-words.append(TLWord(0x29d0e, 4, "Priest"))
+words.append(TLWord(0x29d0e, 4, "Priest "))
 words.append(TLWord(0x29d16, 5, "Tarot"))
-words.append(TLWord(0x29d1f, 8, "Nobility"))
+words.append(TLWord(0x29d1f, 8, "Nobility "))
 
 words.append(TLWord(0x29a34, 17, "Choose a deity:"))
-words.append(TLWord(0x29a24, 15, "Select spellsong(s):"))
-words.append(TLWord(0x29a56, 18, "Is this OK?          Yes    No"))
+words.append(TLWord(0x29a24, 15, "Select spellsongs:"))
+words.append(TLWord(0x29a56, 18, "Is this OK?        Y       N "))
 
-words.append(TLWord(0x16415, 10, "Adventure on"))
-words.append(TLWord(0x1f10d, 12, "Who will search?"))
+words.append(TLWord(0x16415, 10, "Adventure on "))
+words.append(TLWord(0x1f10d, 12, "Who will search? "))
 words.append(TLWord(0x1f11a, 10, " cannot search."))
 words.append(TLWord(0x1ef96, 24, "There's nothing there.\x0f"))
 words.append(TLWord(0x19f06, 21,  "No spellsongs for out of \x01btl"))
@@ -146,21 +146,21 @@ words.append(TLWord(0x19c67, 8,  "Holy Magic"))
 words.append(TLWord(0x19dd8, 15,  "Not wielding a staff!"))
 words.append(TLWord(0x19ef1, 15,  "Need an instrument!"))
 
-words.append(TLWord(0x2992b, 16, "Enter your name:\x03\x03\x4d\x03"))
+words.append(TLWord(0x2992b, 16, "Enter your name: "))
 words.append(TLWord(0x1b1d0, 4, "Tgt:"))
 
 words.append(TLWord(0x678a, 3,  "Elf"))
 words.append(TLWord(0x6786, 3,  "Hum"))
-words.append(TLWord(0x678e, 5,  "Dwarf"))
-words.append(TLWord(0x6794, 8,  "Grassrunner"))
+words.append(TLWord(0x678e, 5,  "Dwarf "))
+words.append(TLWord(0x6794, 8,  "  Grassrunner"))
 words.append(TLWord(0x6771, 4, "Male"))#\x0fFem"))
 words.append(TLWord(0x6776, 3, "Fem"))
 words.append(TLWord(0x67b3, 4, "Level")) # 10 spaces
-words.append(TLWord(0x299a1, 16, "Choose a gender:"))
+words.append(TLWord(0x299a1, 16, "Choose a gender: "))
 words.append(TLWord(0x15ea8, 16, "No save data!"))
 words.append(TLWord(0x159e0, 16, "No save data!"))
 words.append(TLWord(0x1601b, 14, "Copy adventure"))
-words.append(TLWord(0x15ff8, 17, "Select destination"))
+words.append(TLWord(0x15ff8, 17, "Select destination "))
 words.append(TLWord(0x1600a, 16, "Data already exists."))
 words.append(TLWord(0x15aea, 16, "Data already exists."))
 #bard:
@@ -185,19 +185,52 @@ words.append(TLWord(0x1bd75, 3, "Ea"))
 words.append(TLWord(0x1bd7b, 2, "F"))
 words.append(TLWord(0x1bd95, 4, "other"))
 words.append(TLWord(0xa975, 1, "s"))
-words.append(TLWord(0x2ce3f, 7, "Leave Oran"))
-words.append(TLWord(0x2ce83, 8, "Back to Oran"))
-words.append(TLWord(0x2cad5, 14, "Gate to Anc.Kg Inn"))
-words.append(TLWord(0x2cae4, 13, "Brilliant Home Inn"))
-words.append(TLWord(0x2cb5d, 11, "Sorcerers Guild"))
+
+words.append(TLWord(0x2ce3f, 7, "Leave Oran "))
+words.append(TLWord(0x2ce83, 8, "Back to Oran "))
+
+words.append(TLWord(0x2cab7, 3, "Oran"))
+words.append(TLWord(0x2cabb, 4, "Pada"))
+words.append(TLWord(0x2cac0, 4, "Kasv"))
+words.append(TLWord(0x2cac5, 4, "Hope"))
+words.append(TLWord(0x2caca, 5, "Namgo"))
+words.append(TLWord(0x2cad0, 4, "Rex"))
+words.append(TLWord(0x2cad5, 14, "Gate to Anc.Kg "))
+words.append(TLWord(0x2cae4, 13, "Brilliant Home "))
+words.append(TLWord(0x2caf2, 5, "Tower"))
 words.append(TLWord(0x2caf8, 9, "Thief Guild"))
 words.append(TLWord(0x2cb02, 8, "Pharis Tpl"))
 words.append(TLWord(0x2cb0b, 8, "Marfa Tpl"))
 words.append(TLWord(0x2cb14, 8, "Myrii Tpl"))
 words.append(TLWord(0x2cb1d, 8, "Rahda Tpl"))
 words.append(TLWord(0x2cb26, 9, "Cha-Za Tpl"))
-words.append(TLWord(0x2cb30, 7, "Wpn Shop"))
-words.append(TLWord(0x2cb38, 7, "Mir Shop"))
+words.append(TLWord(0x2cb30, 7, "Wpn Shop "))
+words.append(TLWord(0x2cb38, 7, "Mir Shop "))
+words.append(TLWord(0x2cb40, 2, "P "))
+words.append(TLWord(0x2cb43, 10, "RuinsGuard Inn"))
+words.append(TLWord(0x2cb4e, 14, "Uncovered Trsr. Inn"))
+words.append(TLWord(0x2cb69, 8, "Duck Inn"))
+words.append(TLWord(0x2cb72, 6, "Fire Inn"))
+words.append(TLWord(0x2cb79, 5, "RvrInn"))
+words.append(TLWord(0x2cb7f, 6, "Torga"))
+words.append(TLWord(0x2cb86, 10, "Barracks"))
+words.append(TLWord(0x2cb91, 7, "Korshi's"))
+words.append(TLWord(0x2cb99, 9, "Coastal Cave"))
+words.append(TLWord(0x2cb5d, 11, "Sorcerers Guild"))
+words.append(TLWord(0x2cba3, 5, "Market"))
+words.append(TLWord(0x2cba9, 9, "Cha-Za C"))
+words.append(TLWord(0x2cbb3, 8, "Slum Bar"))
+words.append(TLWord(0x2cbbc, 6, "Trade:"))
+words.append(TLWord(0x2cbc3, 10, "Osborne Mns"))
+words.append(TLWord(0x2cbce, 4, "Hwy"))
+words.append(TLWord(0x2cbd3, 3, "Sl"))
+words.append(TLWord(0x2cbd7, 6, "W.Tower"))
+words.append(TLWord(0x2cbde, 10, "Border Ruins"))
+words.append(TLWord(0x2cbe9, 6, "Ein's"))
+words.append(TLWord(0x2cbf0, 4, "Sewer"))
+words.append(TLWord(0x2cbf5, 8, "Mansion"))
+words.append(TLWord(0x2cbfe, 5, "Hdout"))
+words.append(TLWord(0x2cc04, 10, "Garud's Estate"))
 
 words.append(TLWord(0xd802, 17, "Welcome. \x01How can I help?"))
 words.append(TLWord(0xd855, 17, "Welcome. \x01How can I help?"))
@@ -208,9 +241,9 @@ words.append(TLWord(0xdd17, 17, "Welcome. \x01How can I help?"))
 words.append(TLWord(0xdd9f, 17, "Welcome. \x01How can I help?"))
 words.append(TLWord(0xddde, 17, "Welcome. \x01How can I help?"))
 
-words.append(TLWord(0xaa38, 24, "Find  Work\x01Rest \x01Level Up"))
+words.append(TLWord(0xaa38, 24, "Find  Work \x01Rest \x01Level Up "))
 words.append(TLWord(0xaac8, 48, "First gather some allies. \x01I can't give work to less \x01than 5 companions."))
-words.append(TLWord(0xab3d, 14, "No work for now."))
+words.append(TLWord(0xab3d, 14, "No work for now. "))
 words.append(TLWord(0xa806, 9, "Rest? "))
 words.append(TLWord(0xa810, 10, "Another night?"))
 words.append(TLWord(0xa942, 8, "'s HP \x0f"))
@@ -250,7 +283,7 @@ words.append(TLWord(0x3b5e5, 17, "Toss item \x01Toss equipment"))
 words.append(TLWord(0x3b79a, 16, "\x01tossed. \x01Is this OK?"))
 words.append(TLWord(0x3b7ab, 23, " pieces of equip. \x01Toss which one?"))
 words.append(TLWord(0x3b7c5, 10, "No equipment."))
-words.append(TLWord(0x4dd2, 11, ": Add to party?"))
+words.append(TLWord(0x4dd2, 11, ": Recruit? "))
 
 #words.append(TLWord(0x605f, 4, "Bart"))
 #words.append(TLWord(0x6064, 4, "Loox"))
@@ -263,11 +296,11 @@ words.append(TLWord(0x15554, 7, "Music"))
 words.append(TLWord(0x1555c, 11, "Swap Buttons"))
 words.append(TLWord(0x15567, 4, "  Back"))
 words.append(TLWord(0x1556b, 4, "OK"))
-words.append(TLWord(0x1556f, 4, "Stereo"))
+words.append(TLWord(0x1556f, 4, "Ster"))
 words.append(TLWord(0x15573, 4, "Mono"))
 
-words.append(TLWord(0x18f29, 46, "Attack Adj.  1H     2H \x01Strike Adj.  1H     2H \x01Critical num"))
-words.append(TLWord(0x18f58, 8, "Throwable"))
+words.append(TLWord(0x18f29, 46, "Attack Adj.1H     2H \x01Strike Adj.1H     2H \x01Critical # "))
+words.append(TLWord(0x18f58, 8, "(Throw) "))
 words.append(TLWord(0x18fa8, 19, "Attack Adj. \x01Evade Adj."))
 words.append(TLWord(0x19017, 29, "Evade Adj. \x01DFP Adj. \x01Damage Red."))
 words.append(TLWord(0x18e42, 9, "Unequipped"))
@@ -283,6 +316,40 @@ words.append(TLWord(0x1c576, 10, "Expand dmg"))
 words.append(TLWord(0x1c582, 12, "Expand dur"))
 words.append(TLWord(0x1c590, 10, "Inc. success"))
 words.append(TLWord(0x1c59c, 10, "MP Given"))
+words.append(TLWord(0x3843c, 26, "Buy item \x01Sell item\x01Instruments"))
+words.append(TLWord(0x3859c, 15, "Harp \x01Lute \x01Flute"))
+words.append(TLWord(0x385ac, 10, "Sell which?"))
+words.append(TLWord(0x385b8, 20, "Buy who's mage \x01staff?"))
+words.append(TLWord(0x385cd, 8, "Mage Staff"))
+words.append(TLWord(0x385d6, 34, " does not have\x01Sorcerer, so will be used\x01as a weapon."))
+words.append(TLWord(0x385fb, 14, "Nothing to sell"))
+words.append(TLWord(0x3860c, 11, "G total. OK? "))
+words.append(TLWord(0x38933, 13, "Thank you. "))
+words.append(TLWord(0x38941, 9, "\x01Purchase?"))
+words.append(TLWord(0x38951, 5, "Harp "))
+words.append(TLWord(0x38957, 4, "Lute "))
+words.append(TLWord(0x3895c, 4, "Flute"))
+words.append(TLWord(0x38967, 5, "MagStn"))
+words.append(TLWord(0x3896d, 9, "Wizard Heart"))
+words.append(TLWord(0x38994, 16, " will be\x01tossed. Sure?"))
+words.append(TLWord(0x389a5, 17, "\x01Toss something to\x01make room?"))
+words.append(TLWord(0x389b7, 7, " obtained."))
+words.append(TLWord(0x389bf, 7, " obtained."))
+words.append(TLWord(0x389c9, 6, " left."))
+words.append(TLWord(0x389d8, 6, " left."))
+words.append(TLWord(0x386b0, 18, "Arrow (24) \x01Bow ammo."))
+words.append(TLWord(0x386c3, 23, "Quarrel (24) \x01For crossbows. "))
+words.append(TLWord(0x386e1, 23, "MagStn (10) \x01Stone holding MP. "))
+words.append(TLWord(0x386f9, 23, "MagStn (20) \x01Stone holding MP. "))
+words.append(TLWord(0x38711, 23, "Wizard Heart \x01+1 to mental power. "))
+words.append(TLWord(0x38735, 23, "MagStn (30) \x01Stone holding MP. "))
+words.append(TLWord(0x3874d, 31, "Magic Yarn \x01Exit from dungeons."))
+words.append(TLWord(0x3876d, 34, "Warrior's Medicine \x01Raises ATP as HP\x01decreases."))
+words.append(TLWord(0x38790, 16, "Antidote \x01Cures poison."))
+words.append(TLWord(0x387a1, 19, "Tonic\x01Returns consciousness. "))
+words.append(TLWord(0x387b5, 15, "Heal Stone\x01Heals wounds."))
+words.append(TLWord(0xd8a8, 37, "Welcome to the Sorcerers\x01Guild. How can I help?"))
+
 
 print(" OK.")
 
@@ -359,6 +426,10 @@ print("Creating combination chars... ", end="")
 for word in words:
     i = 0
     while i < len(word.translation)-1:
+        if word.translation[i] >= 'A':
+            if word.translation[i] <= 'Z':
+                i += 1
+                continue 
         if(word.translation[i] >= ' ') and (word.translation[i+1] >= ' '):
             if(i == len(word.translation)-1):
                 s = word.translation[i] + ' '
@@ -459,6 +530,10 @@ for word in words:
     word.original = word.translation
     newword = ''
     while i < len(word.translation)-1:
+        if(word.translation[i] >= 'A') and (word.translation[i] <= 'Z'):
+            word.translation = word.translation[:i] + chr(ord(word.translation[i]) - 0x11) + word.translation[i+1:]
+            i += 1
+            continue
         if(word.translation[i] >= ' ') and (word.translation[i+1] >= ' '):
             if(i == len(word.translation)-1):
                 s = word.translation[i]+' '
